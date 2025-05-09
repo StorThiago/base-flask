9e8134fc52b9222cb42067ba5816b6b525550f0a
StorThiago <tbpsa@iscte-iul.pt>
2025-05-09 12:18:44 +0100

    fix: production

--- app/app.py
+++ app/app.py
@@ -1,6 +1,5 @@
-# from flask import Flask
 from flask import Flask
-
+import os
 
 app = Flask(__name__)
 
@@ -11,4 +10,10 @@ def home():
 
     
 if __name__ == "__main__":
-    app.run(debug=True)
+    ## EM TESTES
+    # app.run(debug=True)
+    
+    # EM PRODUÇÃO
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host='0.0.0.0', port=port)
+    # app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 10000)))
